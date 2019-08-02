e1b67b97d7479aed1c5dc5a5f9c43bc4457a9c65
Adriana Silva <adriana.silva@norber.com.br>
2019-08-02 09:17:55 -0300

    Init

---
+++ PaymentContext.Domain/ValueObjects/Address.cs
@@ -0,0 +1,26 @@
+using PaymentContext.Shared.ValueObjects;
+
+namespace PaymentContext.Domain.ValueObjects
+{
+    public class Address : ValueObject  {
+        public Address(string street, string number, string neighborhood, string city, string state, string country, string zipCode)
+        {
+            Street = street;
+            Number = number;
+            Neighborhood = neighborhood;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+        }
+
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Neighborhood { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinaturaTest()
+        {
+            var subscription = new Subscription(null);
+
+            // var student = new Student("Adriana", "Carvalho", "482753481", "adriana.carvsilva@gmail.com");
+            // student.AddSubscription(subscription);
+
+
+
+        }
+    }
+}
